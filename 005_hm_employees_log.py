d3081de6a8b66a248ca66411b5a9be2b0e121899
galileoguzman <galileoguzman@gmail.com>
2020-08-22 13:52:42 -0500

    Added homework

---
+++ 005_hm_employees_log.py
@@ -0,0 +1,8 @@
+# Given 10000 logs
+
+# Group logs by employee
+
+# Number of each category `enter_for` a employee has entered
+# i.e. employee 1002 meeting - 52, call - 80
+
+# Show hot hours for enter to the facilities
